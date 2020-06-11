3fc087c02091580a4c754ff6d139ba13fe827cbd
2536048@qq.com <2536048@qq.com>
2020-06-11 17:24:49 +0800

    fafaf

--- pages/profile/profile.vue
+++ pages/profile/profile.vue
@@ -1,15 +1,225 @@
 <template>
-	<slideshow></slideshow>
+	<view class="warp">
+		<slideshow></slideshow>
+		<view class="example-body">
+			<uni-grid :column="3" :show-border="false" :square="false" @change="change">
+				<uni-grid-item v-if="index < 6" v-for="(item ,index) in list" :index="index" :key="index">
+					<view class="grid-item-box">
+						<image class="image" :src="item.url" mode="aspectFill" />
+						<text class="text">{{item.text}}</text>
+						<view v-if="item.badge" class="grid-dot">
+							<uni-badge :text="item.badge" :type="item.type" />
+						</view>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
+		</view> -->
+		<uni-section title="热门推荐" style="color: #007AFF;" type="line"></uni-section>
+		
+	</view>
 </template>
 
 <script>
-	import slideshow from './views/slideshow.vue';
+	import slideshow from './views/slideshow.vue'
+	import uniSection from '@/components/uni-section/uni-section.vue'
+	import uniGrid from '@/components/uni-grid/uni-grid.vue'
+	import uniGridItem from '@/components/uni-grid-item/uni-grid-item.vue'
+	import uniBadge from '@/components/uni-badge/uni-badge.vue'
 	export default {
-		components : {
-			slideshow
+		components: {
+			slideshow,
+			uniSection,
+			uniGrid,
+			uniGridItem,
+			uniBadge
+		},
+		data() {
+			return {
+				dynamicList: [],
+				list: [{
+						url: '/static/images/profile/历史.png',
+						text: '历史'
+					},
+					{
+						url: '/static/images/profile/人文.png',
+						text: '人文'
+					},
+					{
+						url: '/static/images/profile/风貌.png',
+						text: '风貌'
+					}
+				]
+			}
+		},
+		methods: {
+			change(e) {
+				let {
+					index
+				} = e.detail
+				this.list[index].badge && this.list[index].badge++
+
+				uni.showToast({
+					title: `点击第${index+1}个宫格`,
+					icon: 'none'
+				})
+			},
+			add() {
+				if (this.dynamicList.length < 9) {
+					this.dynamicList.push({
+						url: `/static/c${this.dynamicList.length+1}.png`,
+						text: `Grid ${this.dynamicList.length+1}`,
+						color: this.dynamicList.length % 2 === 0 ? '#f5f5f5' : "#fff"
+					})
+				} else {
+					uni.showToast({
+						title: '最多添加9个',
+						icon: 'none'
+					});
+				}
+			},
+			del() {
+				this.dynamicList.splice(this.dynamicList.length - 1, 1)
+			}
 		}
 	}
 </script>
 
 <style>
+	/* 头条小程序组件内不能引入字体 */
+	/* #ifdef MP-TOUTIAO */
+	@font-face {
+		font-family: uniicons;
+		font-weight: normal;
+		font-style: normal;
+		src: url('~@/static/uni.ttf') format('truetype');
+	}
+
+	/* #endif */
+
+	/* #ifndef APP-NVUE */
+	page {
+		display: flex;
+		flex-direction: column;
+		box-sizing: border-box;
+		background-color: #efeff4;
+		min-height: 100%;
+		height: auto;
+	}
+
+	view {
+		font-size: 14px;
+		line-height: inherit;
+	}
+
+	.example {
+		padding: 0 15px 15px;
+	}
+
+	.example-info {
+		padding: 15px;
+		color: #3b4144;
+		background: #ffffff;
+	}
+
+	.example-body {
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		padding: 0;
+		font-size: 14px;
+		background-color: #ffffff;
+	}
+
+	/* #endif */
+	.example {
+		padding: 0 15px;
+	}
+
+	.example-info {
+		/* #ifndef APP-NVUE */
+		display: block;
+		/* #endif */
+		padding: 15px;
+		color: #3b4144;
+		background-color: #ffffff;
+		font-size: 14px;
+		line-height: 20px;
+	}
+
+	.example-info-text {
+		font-size: 14px;
+		line-height: 20px;
+		color: #3b4144;
+	}
+
+
+	.example-body {
+		flex-direction: column;
+		padding: 15px;
+		background-color: #ffffff;
+	}
+
+	.word-btn-white {
+		font-size: 18px;
+		color: #FFFFFF;
+	}
+
+	.word-btn {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		border-radius: 6px;
+		height: 48px;
+		margin: 15px;
+		background-color: #007AFF;
+	}
+
+	.word-btn--hover {
+		background-color: #4ca2ff;
+	}
+	.image {
+		width: 116rpx;
+		height: 118rpx;
+	}
+
+	.text {
+		font-size: 26rpx;
+		margin-top: 10rpx;
+	}
+
+	.example-body {
+		/* #ifndef APP-NVUE */
+		display: block;
+		/* #endif */
+	}
+
+	.grid-dynamic-box {
+		margin-bottom: 15px;
+	}
+
+	.grid-item-box {
+		flex: 1;
+		/* position: relative;
+ */
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
+	}
+
+	.grid-dot {
+		position: absolute;
+		top: 5px;
+		right: 15px;
+	}
+
+	.swiper {
+		height: 420px;
+	}
 </style>
