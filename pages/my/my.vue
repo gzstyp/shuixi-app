0281c65cb30c2fa5b37246f2684ab877ec3742ca
gzstyp <444141300@qq.com>
2020-06-18 15:53:04 +0800

    我的页面

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,210 +1,230 @@
 <template>
-	<view class="center">
-		<view class="logo" @click="goLogin" :hover-class="!login ? 'logo-hover' : ''">
-			<image class="logo-img" :src="login ? uerInfo.avatarUrl :avatarUrl"></image>
-			<view class="logo-title">
-				<text class="uer-name">Hi，{{login ? uerInfo.name : '您未登录'}}</text>
-				<text class="go-login navigat-arrow" v-if="!login">&#xe65e;</text>
+	<view>
+		<view class="header">
+			<view class="bg">
+				<view class="box">
+					<view class="box-hd">
+						<view class="avator">
+							<image src="/static/icon/icon_user_photo.svg" mode="widthFix"></image>
+						</view>
+						<view class="phone-number">羊城西饼</view>
+					</view>
+					<view class="box-bd">
+						<view class="item" hover-class="my-hover-class">
+							<view class="icon"><image src="/static/icon/icon_message.svg"/></view>
+							<view class="text">消息</view>
+						</view>
+						<view class="item" hover-class="my-hover-class">
+							<view class="icon"><image src="/static/icon/icon_favorite.svg"/></view>
+							<view class="text">收藏</view>
+						</view>
+						<view class="item" hover-class="my-hover-class">
+							<view class="icon"><image src="/static/icon/fans_selected.svg"/></view>
+							<view class="text">粉丝</view>
+						</view>
+					</view>
+				</view>
 			</view>
 		</view>
-		<view class="center-list">
-			<view class="center-list-item border-bottom">
-				<text class="list-icon">&#xebd0;</text>
-				<text class="list-text">我的消息</text>
-				<text class="navigat-arrow">&#xe65e;</text>
+		<view class="list-content">
+			<view class="list">
+				<view class="li noborder" >
+					<view class="icon"><image src="/static/icon/icon_card.svg"></image></view>
+					<view class="text">编辑资料</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
 			</view>
-			<view class="center-list-item border-bottom">
-				<text class="list-icon">&#xea24;</text>
-				<text class="list-text">编辑资料</text>
-				<text class="navigat-arrow">&#xe65e;</text>
-			</view>
-		</view>
-		<view class="center-list">
-			<view class="center-list-item border-bottom">
-				<text class="list-icon">&#xe7e1;</text>
-				<text class="list-text">常见问题</text>
-				<text class="navigat-arrow">&#xe65e;</text>
-			</view>
-			<view class="center-list-item border-bottom">
-				<text class="list-icon">&#xe6d2;</text>
-				<text class="list-text">客服中心</text>
-				<text class="navigat-arrow">&#xe65e;</text>
-			</view>
-			<view class="center-list-item border-bottom">
-				<text class="list-icon">&#xea95;</text>
-				<text class="list-text">分享小程序</text>
-				<text class="navigat-arrow">&#xe65e;</text>
-			</view>
-		</view>
-		<view class="center-list">
-			<view class="center-list-item border-bottom">
-				<text class="list-icon">&#xea4a;</text>
-				<text class="list-text">商家入驻</text>
-				<text class="navigat-arrow">&#xe65e;</text>
-			</view>
-			<view class="center-list-item border-bottom">
-				<text class="list-icon">&#xe98a;</text>
-				<text class="list-text">菜谱上传</text>
-				<text class="navigat-arrow">&#xe65e;</text>
+			<view class="list">
+				<view class="li">
+					<view class="icon"><image src="/static/icon/icon_issue.svg"></image></view>
+					<view class="text">常见问题</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
+				<view class="li">
+					<view class="icon"><image src="/static/icon/icon_customer_center.svg"></image></view>
+					<view class="text">客户中心</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
+				<view class="li">
+					<view class="icon"><image src="/static/icon/icon_share_selected.svg"></image></view>
+					<view class="text">分享小程序</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
+				<view class="li" v-if="shops">
+					<view class="icon"><image src="/static/icon/icon_regsiter.svg"></image></view>
+					<view class="text">商家入驻</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
+				<view class="li" v-if="shops">
+					<view class="icon"><image src="/static/icon/icon_cate_upload.svg"></image></view>
+					<view class="text">菜谱上传</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
+				<view class="li" v-if="shops">
+					<view class="icon"><image src="/static/icon/icon_self_control_menu.svg"></image></view>
+					<view class="text">自制菜品</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
+				<view class="li">
+					<view class="icon"><image src="/static/icon/icon_help.svg"></image></view>
+					<view class="text">帮助中心</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
+				<view class="li">
+					<view class="icon"><image src="/static/icon/icon_setting.svg"></image></view>
+					<view class="text">系统设置</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
+				<view class="li">
+					<view class="icon"><image src="/static/icon/icon_opinion.svg"></image></view>
+					<view class="text">意见反馈</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
+				<view class="li">
+					<view class="icon"><image src="/static/icon/icon_about.svg"></image></view>
+					<view class="text">关于我们</view>
+					<image class="to" src="/static/icon/icon_to_normal.svg"></image>
+				</view>
 			</view>
 		</view>
 	</view>
 </template>
-
 <script>
 	export default {
 		data() {
 			return {
-				login: false,
-				avatarUrl: "../../static/uni-center/logo.png",
-				uerInfo: {}
+				"shops" : false
 			}
 		},
+		onLoad() {
+		},
 		methods: {
-			goLogin() {
-				if (!this.login) {
-					console.log("点击前往登录")
-				}
-			}
+			changeSkin(){
+			},
 		}
 	}
 </script>
 
-<style>
-	@font-face {
-		font-family: texticons;
-		font-weight: normal;
-		font-style: normal;
-		src: url('https://at.alicdn.com/t/font_984210_5cs13ndgqsn.ttf') format('truetype');
+<style lang="scss">
+	.my-hover-class{
+		color:#FEB82B;
 	}
-	
-	@font-face {
-	  font-family: 'iconfont';
-	  src: url('/static/iconfont/iconfont.eot'); /* IE9*/
-	  src: url('/static/iconfont/iconfont.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-	  url('/static/iconfont/iconfont.woff') format('woff'), /* chrome、firefox */
-	  url('/static/iconfont/iconfont.ttf') format('truetype'), /* chrome、firefox、opera、Safari, Android, iOS 4.2+*/
-	  url('/static/iconfont/iconfont.svg#iconfont') format('svg'); /* iOS 4.1- */
+	page{
+		background-color:#f1f1f1;
+		font-size: 30rpx;
 	}
-	
-	.iconfont{
-	    font-family:"iconfont" !important;
-	    font-size:16px;font-style:normal;
-	    -webkit-font-smoothing: antialiased;
-	    -webkit-text-stroke-width: 0.2px;
-	    -moz-osx-font-smoothing: grayscale;}
-
-	page,
-	view {
-		display: flex;
+	.header{
+		background: #fff;
+		height: 290rpx;
+		padding-bottom: 110rpx;
+		.bg{
+			width: 100%;
+			height:200rpx;
+			padding-top:100rpx;
+			background-color:#FEB82B;
+		}
 	}
-
-	page {
-		background-color: #f8f8f8;
+	.box{
+		width: 650rpx;
+		height: 280rpx;
+		border-radius: 20rpx;
+		margin: 0 auto;
+		background: #fff;
+		box-shadow: 0 5rpx 20rpx 0rpx rgba(0, 0, 150, .2); 
+		.box-hd{
+			display: flex;
+			flex-wrap: wrap;
+			flex-direction: row;
+			justify-content: center;
+			.avator{
+				width: 160rpx;
+				height: 160rpx;
+				background: #fff;
+				border: 5rpx solid #fff;
+				border-radius: 50%;
+				margin-top: -80rpx;
+				overflow: hidden;
+				img{
+					width: 100%;
+					height: 100%;
+				}
+			}
+			.phone-number{
+				width: 100%;
+				text-align: center;
+			}
+		}
+		.box-bd{
+			display: flex;
+			flex-wrap: nowrap;
+			flex-direction: row;
+			justify-content: center;
+			.item{
+				flex: 1 1 auto;
+				display: flex;
+				flex-wrap: wrap;
+				flex-direction: row;
+				justify-content: center;
+				border-right: 1px solid #f1f1f1;
+				margin: 15rpx 0;
+				&:last-child{
+					border: none;
+				}
+				.icon{
+					width: 80rpx;
+					height: 80rpx;
+					image{
+						width: 100%;
+						height: 100%;
+					}
+				}
+				.text{
+					width: 100%;
+					text-align: center;
+					margin-top: 10rpx;
+				}
+			}
+		}
 	}
-
-	.center {
-		flex-direction: column;
+	.list-content{
+		background: #ffffff;
 	}
-
-	.logo {
-		width: 750upx;
-		height: 240upx;
-		padding: 20upx;
-		box-sizing: border-box;
-		background-color: #FEB82B;
-		flex-direction: row;
-		align-items: center;
-	}
-
-	.logo-hover {
-		opacity: 0.8;
-	}
-
-	.logo-img {
-		width: 150upx;
-		height: 150upx;
-		border-radius: 150upx;
-	}
-
-	.logo-title {
-		height: 150upx;
-		flex: 1;
-		align-items: center;
-		justify-content: space-between;
-		flex-direction: row;
-		margin-left: 20upx;
-	}
-
-	.uer-name {
-		height: 60upx;
-		line-height: 60upx;
-		font-size: 38upx;
-		color: #FFFFFF;
-	}
-
-	.go-login.navigat-arrow {
-		font-size: 38upx;
-		color: #FFFFFF;
-	}
-
-	.login-title {
-		height: 150upx;
-		align-items: self-start;
-		justify-content: center;
-		flex-direction: column;
-		margin-left: 20upx;
-	}
-
-	.center-list {
-		background-color: #FFFFFF;
-		margin-top: 20upx;
-		width: 750upx;
-		flex-direction: column;
-	}
-
-	.center-list-item {
-		height: 90upx;
-		width: 750upx;
-		box-sizing: border-box;
-		flex-direction: row;
-		padding: 0upx 20upx;
-	}
-
-	.border-bottom {
-		border-bottom-width: 0.5upx;
-		border-color: #c8c7cc;
-		border-bottom-style: solid;
-	}
-
-	.list-icon {
-		width: 40upx;
-		height: 90upx;
-		line-height: 90upx;
-		font-size: 45upx;
-		color: #FEB82B;
-		text-align: center;
-		font-family: iconfont;
-		margin-right: 20upx;
-	}
-
-	.list-text {
-		height: 90upx;
-		line-height: 90upx;
-		font-size: 33upx;
-		color: #555;
-		margin-left: 10upx;
-		flex: 1;
-		text-align: left;
-	}
-
-	.navigat-arrow {
-		height: 90upx;
-		width: 40upx;
-		line-height: 90upx;
-		font-size: 34upx;
-		color: #555;
-		text-align: right;
-		font-family: texticons;
+	.list{
+		width:100%;
+		border-bottom:15rpx solid  #f1f1f1;
+		background: #ffffff;
+		&:last-child{
+			border: none;
+		}
+		.li{
+			width:92%;
+			height:100rpx;
+			padding:0 4%;
+			border-bottom:1px solid rgb(243,243,243);
+			display:flex;
+			align-items:center;
+		&.noborder{
+			border-bottom:0
+			}
+			.icon{
+				flex-shrink:0;
+				width:50rpx;
+				height:50rpx;
+				image{
+					width:50rpx;
+					height:50rpx;
+				}
+			}
+			.text{
+				padding-left:20rpx;
+				width:100%;
+				color:#666;
+			}
+			.to{
+				flex-shrink:0;
+				width:40rpx;
+				height:40rpx;
+			}
+		}
 	}
 </style>
---
+++ pages/my/my_bak.vue
@@ -0,0 +1,210 @@
+<template>
+	<view class="center">
+		<view class="logo" @click="goLogin" :hover-class="!login ? 'logo-hover' : ''">
+			<image class="logo-img" :src="login ? uerInfo.avatarUrl :avatarUrl"></image>
+			<view class="logo-title">
+				<text class="uer-name">Hi，{{login ? uerInfo.name : '您未登录'}}</text>
+				<text class="go-login navigat-arrow" v-if="!login">&#xe65e;</text>
+			</view>
+		</view>
+		<view class="center-list">
+			<view class="center-list-item border-bottom">
+				<text class="list-icon">&#xebd0;</text>
+				<text class="list-text">我的消息</text>
+				<text class="navigat-arrow">&#xe65e;</text>
+			</view>
+			<view class="center-list-item border-bottom">
+				<text class="list-icon">&#xea24;</text>
+				<text class="list-text">编辑资料</text>
+				<text class="navigat-arrow">&#xe65e;</text>
+			</view>
+		</view>
+		<view class="center-list">
+			<view class="center-list-item border-bottom">
+				<text class="list-icon">&#xe7e1;</text>
+				<text class="list-text">常见问题</text>
+				<text class="navigat-arrow">&#xe65e;</text>
+			</view>
+			<view class="center-list-item border-bottom">
+				<text class="list-icon">&#xe6d2;</text>
+				<text class="list-text">客服中心</text>
+				<text class="navigat-arrow">&#xe65e;</text>
+			</view>
+			<view class="center-list-item border-bottom">
+				<text class="list-icon">&#xea95;</text>
+				<text class="list-text">分享小程序</text>
+				<text class="navigat-arrow">&#xe65e;</text>
+			</view>
+		</view>
+		<view class="center-list">
+			<view class="center-list-item border-bottom">
+				<text class="list-icon">&#xea4a;</text>
+				<text class="list-text">商家入驻</text>
+				<text class="navigat-arrow">&#xe65e;</text>
+			</view>
+			<view class="center-list-item border-bottom">
+				<text class="list-icon">&#xe98a;</text>
+				<text class="list-text">菜谱上传</text>
+				<text class="navigat-arrow">&#xe65e;</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				login: false,
+				avatarUrl: "/static/uni-center/logo.png",
+				uerInfo: {}
+			}
+		},
+		methods: {
+			goLogin() {
+				if (!this.login) {
+					console.log("点击前往登录")
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	@font-face {
+		font-family: texticons;
+		font-weight: normal;
+		font-style: normal;
+		src: url('https://at.alicdn.com/t/font_984210_5cs13ndgqsn.ttf') format('truetype');
+	}
+	
+	@font-face {
+	  font-family: 'iconfont';
+	  src: url('/static/iconfont/iconfont.eot'); /* IE9*/
+	  src: url('/static/iconfont/iconfont.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+	  url('/static/iconfont/iconfont.woff') format('woff'), /* chrome、firefox */
+	  url('/static/iconfont/iconfont.ttf') format('truetype'), /* chrome、firefox、opera、Safari, Android, iOS 4.2+*/
+	  url('/static/iconfont/iconfont.svg#iconfont') format('svg'); /* iOS 4.1- */
+	}
+	
+	.iconfont{
+	    font-family:"iconfont" !important;
+	    font-size:16px;font-style:normal;
+	    -webkit-font-smoothing: antialiased;
+	    -webkit-text-stroke-width: 0.2px;
+	    -moz-osx-font-smoothing: grayscale;}
+
+	page,
+	view {
+		display: flex;
+	}
+
+	page {
+		background-color: #f8f8f8;
+	}
+
+	.center {
+		flex-direction: column;
+	}
+
+	.logo {
+		width: 750upx;
+		height: 240upx;
+		padding: 20upx;
+		box-sizing: border-box;
+		background-color: #FEB82B;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.logo-hover {
+		opacity: 0.8;
+	}
+
+	.logo-img {
+		width: 150upx;
+		height: 150upx;
+		border-radius: 150upx;
+	}
+
+	.logo-title {
+		height: 150upx;
+		flex: 1;
+		align-items: center;
+		justify-content: space-between;
+		flex-direction: row;
+		margin-left: 20upx;
+	}
+
+	.uer-name {
+		height: 60upx;
+		line-height: 60upx;
+		font-size: 38upx;
+		color: #FFFFFF;
+	}
+
+	.go-login.navigat-arrow {
+		font-size: 38upx;
+		color: #FFFFFF;
+	}
+
+	.login-title {
+		height: 150upx;
+		align-items: self-start;
+		justify-content: center;
+		flex-direction: column;
+		margin-left: 20upx;
+	}
+
+	.center-list {
+		background-color: #FFFFFF;
+		margin-top: 20upx;
+		width: 750upx;
+		flex-direction: column;
+	}
+
+	.center-list-item {
+		height: 90upx;
+		width: 750upx;
+		box-sizing: border-box;
+		flex-direction: row;
+		padding: 0upx 20upx;
+	}
+
+	.border-bottom {
+		border-bottom-width: 0.5upx;
+		border-color: #c8c7cc;
+		border-bottom-style: solid;
+	}
+
+	.list-icon {
+		width: 40upx;
+		height: 90upx;
+		line-height: 90upx;
+		font-size: 45upx;
+		color: #FEB82B;
+		text-align: center;
+		font-family: iconfont;
+		margin-right: 20upx;
+	}
+
+	.list-text {
+		height: 90upx;
+		line-height: 90upx;
+		font-size: 33upx;
+		color: #555;
+		margin-left: 10upx;
+		flex: 1;
+		text-align: left;
+	}
+
+	.navigat-arrow {
+		height: 90upx;
+		width: 40upx;
+		line-height: 90upx;
+		font-size: 34upx;
+		color: #555;
+		text-align: right;
+		font-family: texticons;
+	}
+</style>
